e3b73fc2c1ddc200d342428ba2cb00c436588e60
SomneelMaity <somneel.maity@gmail.com>
2024-05-27 20:27:44 +0530

    Create Music Player

--- QTify/src/App.jsx
+++ QTify/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar";
-
+import Player from "./Components/Player";
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
       </div>
+      <Player />
     </div>
   );
 };
