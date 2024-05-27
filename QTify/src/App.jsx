2ed6a3f5b209308e627aa8fb8531bcdd84ed867f
SomneelMaity <somneel.maity@gmail.com>
2024-05-27 19:52:04 +0530

    Sidebar UI

--- QTify/src/App.jsx
+++ QTify/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./Components/Sidebar";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
